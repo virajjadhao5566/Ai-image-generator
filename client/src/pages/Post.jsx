8ea0dc9d91d5d63190d6d1566a3cf1cb67fb05d7
Viraj <virajjadhao85@gmail.com>
2023-09-05 19:07:48 +0530

    Created a basic setup

---
+++ client/src/pages/Post.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>
+      <h1>Hello from Post</h1>
+    </div>
+  )
+}
+
+export default Post
---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import Post from "./Post";
+export{
+    Home,Post
+}
